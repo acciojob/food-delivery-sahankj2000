313628958feb6cd435bcf937ca725a7a8bf55bf1
sahan <sahan.k.j2000@gmail.com>
2022-12-30 16:58:10 +0530

    Third Commit

--- src/main/java/com/driver/shared/dto/FoodDto.java
+++ src/main/java/com/driver/shared/dto/FoodDto.java
@@ -1,10 +1,14 @@
 package com.driver.shared.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodDto{
 
 	private long id;
